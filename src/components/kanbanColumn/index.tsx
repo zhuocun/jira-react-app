c862ab64e8982eed63ac4c922bbcd313913d8664
zhuocun <zhuocun.tang@outlook.com>
2022-11-29 01:26:04 +1100

    style: task & bug icon

--- src/components/kanbanColumn/index.tsx
+++ src/components/kanbanColumn/index.tsx
@@ -1,17 +1,45 @@
 import useReactQuery from "../../utils/hooks/useReactQuery";
+import taskIcon from "../../assets/task.svg";
+import bugIcon from "../../assets/bug.svg";
+import styled from "@emotion/styled";
+import { Card } from "antd";
 
 const KanbanColumn: React.FC<{ kanban: IKanban }> = ({ kanban }) => {
     const { data: tasks } = useReactQuery<ITask[]>("tasks", {
         kanbanId: kanban._id
     });
     return (
-        <div>
+        <Container>
             <h3>{kanban.kanbanName}</h3>
-            {tasks?.map((task, index) => (
-                <div key={index}>{task.taskName}</div>
-            ))}
-        </div>
+            <TaskContainer>
+                {tasks?.map((task, index) => (
+                    <Card key={index} style={{ marginBottom: "0.5rem" }}>
+                        <div>{task.taskName}</div>
+                        <img src={task.type === "Task" ? taskIcon : bugIcon} />
+                    </Card>
+                ))}
+            </TaskContainer>
+        </Container>
     );
 };
 
 export default KanbanColumn;
+
+const Container = styled.div`
+  min-width: 27rem;
+  border-radius: 6px;
+  background-color: rgb(244, 245, 247);
+  display: flex;
+  flex-direction: column;
+  padding: 0.7rem;
+  margin-right: 1.5rem
+`;
+
+const TaskContainer = styled.div`
+  overflow: scroll;
+  flex: 1;
+
+  ::-webkit-scrollbar {
+    display: none;
+  }
+`;
