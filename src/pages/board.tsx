7de88fb267a44f96db5001731d54d8cec96f1df4
zhuocun <zhuocun.tang@outlook.com>
2022-12-04 19:12:50 +1100

    style: spin position of board

--- src/pages/board.tsx
+++ src/pages/board.tsx
@@ -113,6 +113,6 @@ export const ColumnContainer = styled.div`
 
 const BoardSpin = styled(Spin)`
     margin-left: calc(0.5 * (100vw - 16rem - 26.4rem));
-    margin-top: calc(0.5 * (100vh - 6rem - 40.6rem));
+    margin-top: calc(0.5 * (100vh - 6rem - 40.7rem));
     padding: 1rem;
 `;
