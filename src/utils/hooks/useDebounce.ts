407cb63d6f4ab7c232c8404424f9c94f60eca1d4
zhuocun <zhuocun.tang@outlook.com>
2023-02-09 18:00:01 +1100

    chore: update comments of useDebounce

--- src/utils/hooks/useDebounce.ts
+++ src/utils/hooks/useDebounce.ts
@@ -7,7 +7,7 @@ const useDebounce = <P>(param: P, delay: number): P => {
         const timeout = setTimeout(() => setDebouncedParam(param), delay);
         // after the param or delay is changed, a new useEffect will be scheduled to run, at the same time,
         // the return below of the old useEffect will execute to clean the old timeout.
-        // this makes sure that, during the continuously input of the user, the timeout will always be reset,
+        // this makes sure that, during the continuous input of the user, the timeout will always be reset,
         // so, the setDebouncedParam(param) won't fire, and the re-render of this component won't fire,
         // so that the 'return debouncedParam' won't fire
         // until the user finishes the input.
